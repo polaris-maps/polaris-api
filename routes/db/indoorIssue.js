ad1f49d0d2f5ff760be8f87fb24032256dc5553c
skywang1234 <32024922+skywang1234@users.noreply.github.com>
2024-10-08 01:53:00 -0400

    initial commit

--- routes/db/indoorIssue.js
+++ routes/db/indoorIssue.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const multer = require("multer");
+
+// In memory storage with multer 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // indoorIssueRoutes is an instance of the express router.
 // We use it to define our routes.
@@ -53,17 +58,24 @@ indoorIssueRoutes.get("/app/indoorIssue/:id", async (req, res, next) => {
 });
 
 // Create a new indoorIssue.
-indoorIssueRoutes.route("/app/indoorIssue/add").post(async (req, res, next) => {
+indoorIssueRoutes.route("/app/indoorIssue/add").post(upload.single('image'), async (req, res, next) => {
     const { avoidPolygon, location, latitude, longitude, description, status, datetimeOpen, datetimeClosed, datetimePermanent, votes } = req.body;
 
+    let imageEncoded = null;
+
+    if (req.file) {
+        // Encode the image to Base64
+        imageEncoded = req.file.buffer.toString('base64');
+    }
+    
     const queryText = `
-        INSERT INTO Issue(avoidPolygon, location, latitude, longitude, description, status, datetimeOpen, datetimeClosed, datetimePermanent, votes)
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+        INSERT INTO Issue(avoidPolygon, location, latitude, longitude, description, status, datetimeOpen, datetimeClosed, datetimePermanent, votes, image)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
         RETURNING *;
     `;
 
     try {
-        const { rows } = await pool.query(queryText, [avoidPolygon, location, latitude, longitude, description, status, datetimeOpen, datetimeClosed, datetimePermanent, votes || 0]);
+        const { rows } = await pool.query(queryText, [avoidPolygon, location, latitude, longitude, description, status, datetimeOpen, datetimeClosed, datetimePermanent, votes || 0, imageEncoded]);
         res.status(200).json({
             message: "Successfully added indoor issue",
             data: rows[0]
@@ -74,11 +86,18 @@ indoorIssueRoutes.route("/app/indoorIssue/add").post(async (req, res, next) => {
 });
 
 // Update an indoorIssue by id.
-indoorIssueRoutes.route("/app/indoorIssue/update/:id").patch(async (req, res, next) => {
+indoorIssueRoutes.route("/app/indoorIssue/update/:id").patch(upload.single('image'), async (req, res, next) => {
     const issueId = req.params.id;
     const updates = req.body;
     const keys = Object.keys(updates);
     const values = Object.values(updates);
+
+    if (req.file) {
+        updates.image = req.file.buffer.toString('base64');
+        keys.push('image');
+        values.push(updates.image);
+    }
+
     const setClause = keys.map((key, index) => `"${key}" = $${index + 2}`).join(', ');
 
     const queryText = `UPDATE Issue SET ${setClause} WHERE issue_id = $1 RETURNING *`;
